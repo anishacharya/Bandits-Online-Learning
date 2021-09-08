8937b7af8d669a052e2600b12eea420f3ae4a541
Anish Acharya <anishacharya@utexas.edu>
2021-09-08 02:40:41 -0500

    +

--- src/ETC.py
+++ src/ETC.py
@@ -3,26 +3,18 @@ import pickle
 import math
 
 
-def run(avg, m, num_iter, num_inst):
-    reg = np.zeros((num_inst, num_iter))
-
-    return reg
-
-
 class ETC:
-    def __init__(self, avg, m):  ## Initialization
+    def __init__(self, avg: np.ndarray, explore_steps: int):
 
         self.time = 0
         self.cum_regret = 0
-        self.emp_means = np.zeros_like(avg) # empirical means of arms
-        self.num_pulls = np.zeros_like(avg) # number of times that arm i has been pulled
+        self.emp_means = np.zeros_like(avg)  # empirical means of arms
+        self.num_pulls = np.zeros_like(avg)  # number of times that arm i has been pulled
 
-
-        self.means = avg                    # true means of the arms
-        self.m = m                          # num of explore steps per arm
-        self.num_arms = avg.size            # num arms (k)
-        self.best_arm = np.argmax(avg)      # best arm *
-
+        self.means = avg                     # true means of the arms
+        self.m = explore_steps               # num of explore steps per arm
+        self.num_arms = avg.size             # num arms (k)
+        self.best_arm = np.argmax(avg)       # True best arm
 
     def restart(self):
         # Reset counters
@@ -31,37 +23,41 @@ class ETC:
         self.emp_means = np.zeros_like(self.means)
         self.num_pulls = np.zeros_like(self.means)
 
-
-
     def get_best_arm(self):
-    # For each time index, find the best arm according to ETC.
+        # For each time index, find the best arm according to ETC.
+        return np.argmax(self.emp_means)
 
+    def update_stats(self, rew, arm):
+        pass
 
-    def update_stats(self, rew, arm):  ## Update the empirical means, the number of pulls, and increment the time index
+    def update_reg(self, rew_vec, arm):
+        pass
 
-        ## Your code here
-
-        return None
-
-    def update_reg(self, rew_vec, arm):  ## Update the cumulative regret
-
-        ## Your code here
-
-        return None
-
-    def iterate(self, rew_vec):  ## Iterate the algorithm
-
-        ## Your code here
-
-        return None
+    def iterate(self, rew_vec):
+        pass
 
 
 def get_reward(avg):
     return avg + np.random.normal(len(avg))
 
 
+def run(avg, explore_steps, iterations, num_repeat):
+    regret = np.zeros((num_repeat, iterations))
+    etc = ETC(avg=avg, explore_steps=explore_steps)
+
+    for j in range(num_repeat):
+        etc.restart()
+        for t in range(iterations - 1):
+            rew_vec = get_reward(avg)  # Reward Genie
+            etc.iterate(rew_vec=rew_vec)
+
+        regret[j, :] = np.asarray(etc.cum_regret)
+
+    return regret
+
+
 if __name__ == '__main__':
     mu = np.asarray([0.6, 0.9, 0.95, 0.8, 0.7, 0.3])
     num_iter, num_inst = int(1e4), 30
     m = 1
-    reg = run(avg=mu, m=m, num_iter=num_iter, num_inst=num_inst)
+    reg = run(avg=mu, explore_steps=m, iterations=num_iter, num_repeat=num_inst)
