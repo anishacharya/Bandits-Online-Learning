5ce5afef0dd3bbe73c2eff568dd48fd4f9584248
Anish Acharya <anishacharya@utexas.edu>
2021-09-09 20:14:43 -0500

    +

---
+++ src/epsilon_greedy.py
@@ -0,0 +1,118 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+class EpsilonGreedy:
+    def __init__(self, avg: np.ndarray):
+
+        self.true_means = avg  # true means of the arms
+        sort = np.sort(self.true_means)[::-1]
+        self.delta_min = sort[0] - sort[1]
+        self.C = 1
+
+        self.time = 0
+        self.regret = []
+
+        self.emp_means = np.zeros_like(self.true_means)  # empirical means of arms
+        self.num_pulls = np.zeros_like(self.true_means)  # number of times that arm i has been pulled
+
+        self.num_arms = avg.size  # num arms (k)
+        self.best_arm = int(np.argmax(self.true_means))  # True best arm
+
+        self.arm_ix = None
+
+    def restart(self):
+        # Reset counters
+        self.time = 0
+        self.regret = []
+        self.emp_means = np.zeros_like(self.true_means)
+        self.num_pulls = np.zeros_like(self.true_means)
+        self.arm_ix = None
+
+    def get_best_arm(self):
+        # For each time index, find the best arm according to ETC.
+        return np.argmax(self.emp_means)
+
+    def update_stats(self, rew_vec, explore_flag):
+        ni = self.num_pulls[self.arm_ix]
+
+        # genie plays best arm
+        genie_rew = rew_vec[self.best_arm]
+        player_rew = rew_vec[self.arm_ix]
+        self.regret.append((genie_rew - player_rew))
+
+        if explore_flag == 1:
+            # keep online average
+            self.emp_means[self.arm_ix] = self.emp_means[self.arm_ix] * (ni / (ni + 1)) + player_rew / (ni + 1)
+            self.num_pulls[self.arm_ix] += 1
+
+        self.time += 1
+
+    def get_reward(self):
+        return self.true_means + np.random.normal(0, 1, np.shape(self.true_means))
+
+    def iterate(self):
+        rew_vec = self.get_reward()
+
+        if self.time < self.num_arms:
+            # Pure explore Phase - Round Robin for 1 round
+            self.arm_ix = self.time % self.num_arms
+            explore = 1
+
+        else:
+            # toss a coin
+            epsilon = self.C * self.num_arms / (self.time * self.delta_min ** 2)
+            explore = np.random.binomial(n=1, p=epsilon)
+
+            if explore == 1:
+                # case-1 explore
+                # choose an arm uniformly at random
+                self.arm_ix = np.random.randint(low=0, high=self.num_arms+1)
+            else:
+                # case-2 exploit
+                self.arm_ix = self.get_best_arm()
+
+        self.update_stats(rew_vec=rew_vec, explore_flag=explore)
+
+
+def run(avg, iterations, num_repeat):
+    regret = np.zeros((num_repeat, iterations))
+    etc = EpsilonGreedy(avg=avg)
+    for j in range(num_repeat):
+        np.random.seed(j)
+        for t in range(iterations):
+            etc.iterate()
+
+        # calculate cumulative regret
+        regret[j, :] = np.cumsum(np.asarray(etc.regret))
+        etc.restart()
+
+    return regret
+
+
+if __name__ == '__main__':
+    mu = np.asarray([0.96, 0.7, 0.5, 0.6, 0.1])
+    num_iter, num_inst = int(1e4), 10
+
+    delta = 0.2
+    m = max(1, math.ceil((4 / delta ** 2) * math.log(num_iter * (delta ** 2 / 4))))
+    reg = run(avg=mu,
+              iterations=num_iter,
+              num_repeat=num_inst)
+
+    mean_runs = np.mean(reg, axis=0)
+    std_runs = np.std(reg, axis=0)
+
+    UB = mean_runs + 1 * std_runs
+    LB = mean_runs - 1 * std_runs
+
+    x = np.arange(len(mean_runs))
+    plt.plot(x, mean_runs, color='b')
+    plt.fill_between(x, LB, UB, alpha=0.3, linewidth=0.5, color='b')
+
+    plt.vlines(x=m * len(mu), ymin=0, ymax=1000, linestyle='dashed', color='k')
+    plt.xlabel('Time', fontsize=10)
+    plt.ylabel('Cumulative Regret', fontsize=10)
+    plt.grid(True, which='both', linestyle='--')
+    plt.show()
